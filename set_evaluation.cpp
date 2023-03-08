bec10d3498142c22e615bb12440c49447e6880c4
adesvall <adesvall@student.42.fr>
2023-03-08 17:33:55 +0100

    seteval

--- set_evaluation.cpp
+++ set_evaluation.cpp
@@ -1,10 +1,56 @@
 #include <string>
 #include <vector>
+#include <map>
+#include <algorithm>
 #include <iostream>
+#include "main.hpp"
 
 using namespace std;
 
 vector<int> eval_set(string &formula, vector<vector<int>> &sets)
 {
-    
+    vector<int> result;
+    map<int, bool> indicator;
+    char variables[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    variables[sets.size()] = '\0';
+    for (vector<int> set : sets)
+    {
+        for (int a : set)
+        {
+            if (!indicator.count(a))
+                indicator[a] = false;
+        }
+    }
+
+    for (auto pair : indicator)
+    {
+        int values = 0;
+        for (vector<int> set : sets)
+        {
+            values <<= 1;
+            values &= (find(set.begin(), set.end(), pair.first) != set.end());
+        }
+        string f = replace_var(formula, variables, values);
+        pair.second = eval_formula(f);
+        if (pair.second)
+            result.push_back(pair.first);
+    }
+    return result;
+}
+
+int main()
+{
+    vector<vector<int>> sets;
+    sets.push_back({0, 1, 2, 3, 4});
+    sets.push_back({0, 1, 2, 5, 6});
+    string formula = "AB&";
+
+    vector<int> result = eval_set(formula, sets);
+
+    cout << "{\t";
+    for (int a : result)
+        cout << a << ", ";
+    cout << "\t}," << endl;
+
 }
